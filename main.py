c09e8b988d18cd5714c0cb941e284bdc406af20a
bug132008 <nhaneye@gmail.com>
2022-09-26 16:51:45 -0300

    herpes

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ app = Flask(__name__)
 @app.route('/')
 def home():
 	return render_template("home/index.html")
-@app.route('/<
+@app.route('/<p>')
+def any(p):
+	return f'A página {p} não existe'
 if __name__ == "__main__":
 	app.run(debug=True, port=5500)
