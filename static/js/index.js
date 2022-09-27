7ffb9ff550906660d95d147a06ca88028ee3a05f
bug132008 <nhaneye@gmail.com>
2022-09-27 15:58:15 -0300

    ff

--- static/js/index.js
+++ static/js/index.js
@@ -8,7 +8,8 @@ const hepatC = document.getElementById("hepatC")
 const herpes = document.getElementById("herpe")
 const hpv = document.getElementById("hpv")
 const hivAidsT = document.getElementById("aidsHivT")
-const escondidos = {"Oqe": Oqe, "MaisComuns": MaisComuns, "gonoTxt": gonorr, "cancrom": cancrom, "sifi": sifi, "hepaB": hepatB, "hepaC": hepatC, "herpe": herpes, "hpv": hpv, "aidsHiv": hivAidsT}
+const pres = document.getElementById("pres")
+const escondidos = {"Oqe": Oqe, "MaisComuns": MaisComuns, "gonoTxt": gonorr, "cancrom": cancrom, "sifi": sifi, "hepaB": hepatB, "hepaC": hepatC, "herpe": herpes, "hpv": hpv, "aidsHiv": hivAidsT, "pres": pres}
 function mostrar(obj){
   for(item in escondidos){
     if(item == obj){
